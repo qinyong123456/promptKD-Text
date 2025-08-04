eaf75c24655911f3edd9ce3ed36b420f12b3c222
Jacky Qin <60870424+qinyong123456@users.noreply.github.com>
2025-08-04 15:40:25 +0800

    Update base2new_train.sh

--- promptkd_textrefiner/scripts/textrefiner/base2new_train.sh
+++ promptkd_textrefiner/scripts/textrefiner/base2new_train.sh
@@ -22,7 +22,6 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --seed ${SEED} \
     --trainer ${TRAINER} \
     --dataset-config-file configs/datasets/${DATASET}.yaml \
-    #--config-file configs/trainers/${TRAINER}/${CFG}.yaml \
     --config-file promptkd_textrefiner/configs/trainers/promptkd/${CFG}.yaml \
     --output-dir ${DIR} \
     DATASET.NUM_SHOTS ${SHOTS} \
