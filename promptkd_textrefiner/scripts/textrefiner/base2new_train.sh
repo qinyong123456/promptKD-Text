36748325e97f64aaf0a53569c3ec791697e97419
Jacky Qin <60870424+qinyong123456@users.noreply.github.com>
2025-08-13 16:28:42 +0800

    Update base2new_train.sh

--- promptkd_textrefiner/scripts/textrefiner/base2new_train.sh
+++ promptkd_textrefiner/scripts/textrefiner/base2new_train.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# 检查是否提供了数据集参数
+if [ $# -ne 1 ]; then
+    echo "用法: $0 <数据集名称>"
+    echo "例如: $0 caltech101"
+    echo "支持的数据集: imagenet, caltech101, dtd, eurosat, fgvc_aircraft, oxford_flowers, food101, oxford_pets, sun397, ucf101"
+    exit 1
+fi
+
 # custom config
 DATA='/kaggle/working/promptKD-Text/data'
 TRAINER=TextRefiner
 
-# stanford_cars
-
-DATASET='caltech101' # 'imagenet' 'caltech101' 'dtd' 'eurosat' 'fgvc_aircraft' 'oxford_flowers' 'food101' 'oxford_pets', 'sun397' 'ucf101' 
+# 从命令行参数获取数据集名称
+DATASET=$1
 SEED=1
 
 CFG=vit_b16_c2_ep20_batch8_4+4ctx
@@ -14,8 +21,15 @@ SHOTS=0
 
 DIR=output/base2new/train_base/${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/seed_${SEED}
 
-# fgvc_aircraft, oxford_flowers, dtd: KD_WEIGHT:200
-# imagenet, caltech101, eurosat, food101, oxford_pets, stanford_cars, sun397, ucf101, KD_WEIGHT:1000
+# 根据数据集设置不同的KD_WEIGHT
+case $DATASET in
+    fgvc_aircraft|oxford_flowers|dtd)
+        KD_WEIGHT=200.0
+        ;;
+    *)
+        KD_WEIGHT=1000.0
+        ;;
+esac
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
     --root ${DATA} \
@@ -27,4 +41,5 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     DATASET.NUM_SHOTS ${SHOTS} \
     TRAINER.MODAL base2novel \
     TRAINER.PROMPTKD.TEMPERATURE 1.0 \
-    TRAINER.PROMPTKD.KD_WEIGHT 1000.0
+    TRAINER.PROMPTKD.KD_WEIGHT ${KD_WEIGHT}
+    
